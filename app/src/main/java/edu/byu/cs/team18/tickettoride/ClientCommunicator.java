099e09c08e726b58740d755741c5edcfdbd540df
mattcprice <price.mc1x@gmail.com>
2017-10-09 08:25:03 -0600

    Client Communicator w/o AsyncTask

---
+++ app/src/main/java/edu/byu/cs/team18/tickettoride/ClientCommunicator.java
@@ -0,0 +1,107 @@
+package edu.byu.cs.team18.tickettoride;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.lang.reflect.Constructor;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+
+
+public class ClientCommunicator {
+    private static ClientCommunicator SINGLETON;
+    private static Gson gson = new Gson();
+    private static String SERVER_HOST = "localhost";
+    private static String SERVER_PORT = "8080";
+    private static String HTTP_POST = "POST";
+    private static String HTTP_GET = "GET";
+    private static String URL_PREFIX = "http://" + SERVER_HOST + ":" + SERVER_PORT;
+
+    public Object send(String url, Object command){
+        HttpURLConnection connection = openConnection("/"+url, HTTP_GET, true);
+        sendToServerCommunicator(connection, command);
+        Object result = null;
+        try {
+            result = returnResult(connection, Class.forName("Common."+url+"Command"));
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static ClientCommunicator getSingleton(){
+        if (SINGLETON == null) SINGLETON = new ClientCommunicator();
+        return SINGLETON;
+    }
+
+    private HttpURLConnection openConnection(String contextIdentifier,
+                                             String requestMethod,
+                                             boolean sendingSomthingToServer)
+    {
+        HttpURLConnection result = null;
+        try {
+            URL url = new URL(URL_PREFIX + contextIdentifier);
+            result = (HttpURLConnection)url.openConnection();
+            result.setRequestMethod(requestMethod);
+            result.setDoOutput(sendingSomthingToServer);
+            result.connect();
+        } catch (MalformedURLException e) {
+            System.out.print(e.toString());
+        } catch (IOException e) {
+            System.out.print(e.toString());
+        }
+
+        return result;
+    }
+    private Object returnResult(HttpURLConnection connection, Class<?> klass) {
+        Object result = null;
+        try {
+            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                //0 means the body reServerCommunicator.REGISTER_DESIGNATORsponse length == 0; it was empty
+                if(connection.getContentLength() == 0) {
+                    System.out.println("Response body was empty");
+                } else if(connection.getContentLength() == -1) {
+                    //-1 means the body was not empty but has an unknown about of information
+                    JsonParser jsonParser = new JsonParser();
+                    JsonObject jsonObject = jsonParser.parse(new BufferedReader(new InputStreamReader(connection.getInputStream()))).getAsJsonObject();
+                    result = gson.fromJson(jsonObject, klass);
+
+                }
+            } else {
+                throw new Exception(String.format("http code %d",	connection.getResponseCode()));
+            }
+        } catch (JsonSyntaxException | JsonIOException | IOException e) {
+            System.out.print(e.toString());
+        } catch (Exception e) {
+            System.out.print(e.toString());
+        }
+        return result;
+    }
+
+    private void sendToServerCommunicator(HttpURLConnection connection,
+                                          Object objectToSend)
+    {
+        PrintWriter printWriter = null;
+        try {
+            printWriter = new PrintWriter(connection.getOutputStream());
+            //Encoding in JSON
+            gson.toJson(objectToSend, printWriter);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(printWriter != null) {
+                printWriter.close();
+            }
+        }
+    }
+
+}
