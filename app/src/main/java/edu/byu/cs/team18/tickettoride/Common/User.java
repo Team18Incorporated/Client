96def5c437a78b7336e9262382c9185421e21582
davidasolomon92 <davidasolomon92@gmail.com>
2017-10-10 13:24:03 -0600

    Merge dev and devOberservavle
    merged the dev and devObservavle branches into the combine branch

--- app/src/main/java/edu/byu/cs/team18/tickettoride/Common/User.java
+++ app/src/main/java/edu/byu/cs/team18/tickettoride/Common/User.java
@@ -7,7 +7,7 @@ public class User {
     private AuthToken authToken;
     private String username;
 
-<<<<<<< HEAD
+
     public AuthToken getAuthToken() {
         return authToken;
     }
@@ -28,18 +28,5 @@ public class User {
         this.authToken = authToken;
         this.username = username;
     }
-=======
-    public AuthToken getAuthToken(){
-        return authToken;
-    }
-    public void setAuthToken(AuthToken authIn){ authToken = authIn;}
-    public String getUsername(){ return username;}
-    public void setUsername(String nameIn) {username= nameIn;}
 
-    public User (AuthToken authIn, String nameIn){
-        authToken = authIn;
-        username = nameIn;
-    }
-
->>>>>>> devObservavle
 }
