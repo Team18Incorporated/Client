12757f2bdea87db59c0f88c8d519a0f102e7054e
Antman537 <Antman537@users.noreply.github.com>
2017-10-24 21:27:26 -0600

    added gameActivity and package

---
+++ app/src/main/java/edu/byu/cs/team18/tickettoride/GameView/GameActivity.java
@@ -0,0 +1,15 @@
+package edu.byu.cs.team18.tickettoride.GameView;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.byu.cs.team18.tickettoride.R;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
