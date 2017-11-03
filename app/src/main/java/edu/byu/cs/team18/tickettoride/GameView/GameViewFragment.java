05647f64936d9a5e8615786e21fa6e960cba8123
Antman537 <Antman537@yahoo.com>
2017-11-03 10:04:30 -0600

    routes
    added up to route 13

--- app/src/main/java/edu/byu/cs/team18/tickettoride/GameView/GameViewFragment.java
+++ app/src/main/java/edu/byu/cs/team18/tickettoride/GameView/GameViewFragment.java
@@ -8,6 +8,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -50,7 +51,8 @@ public class GameViewFragment extends Fragment {
     private ImageView faceUpCard5;
 
     private static int[] routeIDs = new int[] {
-            R.id.r1s1,R.id.r1s2,R.id.r2s1,R.id.r2s2
+            R.id.r1s1,R.id.r1s2,
+            R.id.r2s1,R.id.r2s2
     };
 
     public GameViewFragment() {
@@ -187,34 +189,9 @@ public class GameViewFragment extends Fragment {
                 GamePresenter.SINGLETON.incrementTest();
             }
         });
+        initializeRoutes();
         GamePresenter.SINGLETON.setView(this);
         return view;
-    }
-    /*
-    creates a new ImageView for a car at the specified location, and returns a pointer to the ImageView.
-    Should be called by segment creation routine
-    @pre: pos != null && is the position of the car on the map, angle!=null
-    @post: ImageView is initialized, connected, and has its onclick listener set
-     */
-    public ImageView generateCar(Point pos, int angle, Route route){
-        ImageView iv = new ImageView(activity);
-        iv.setImageResource(R.drawable.car_clear);
-        //set car position and add to xml
-        RelativeLayout rl = (RelativeLayout) view.findViewById(R.id.gameViewLayout);
-        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT,
-                RelativeLayout.LayoutParams.WRAP_CONTENT);
-        lp.leftMargin = pos.x;
-        lp.topMargin = pos.y;
-        rl.addView(iv, lp);
-        //rotate car
-        Matrix matrix = new Matrix();
-        iv.setScaleType(ImageView.ScaleType.MATRIX);   //required
-        matrix.postRotate((float) angle, 0, 0);
-        iv.setImageMatrix(matrix);
-        //set onclick listener
-        setCarClick(iv, route);
-        return iv;
     }
 
     private void initializeRoutes(){
@@ -232,11 +209,15 @@ public class GameViewFragment extends Fragment {
     }
 
     private void setCarClick(final ImageView car, final Route route){
-        car.setOnClickListener(new View.OnClickListener() {
+        car.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
+            public boolean onTouch(View view, MotionEvent motionEvent) {
                 if (route.getOwnerID().equals(null)) {
                     GamePresenter.SINGLETON.selectRoute(route);
+                    return true;
+                }
+                else {
+                    return false;
                 }
             }
         });
